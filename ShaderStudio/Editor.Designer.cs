4d1c610c4c1ffc8d7315cb4b840408e5c5875bfe
Jim-Dev <jimmy.toledo.a@gmail.com>
2017-09-10 20:43:36 -0300

    Form layout

--- ShaderStudio/Editor.Designer.cs
+++ ShaderStudio/Editor.Designer.cs
@@ -29,6 +29,16 @@
         private void InitializeComponent()
         {
             this.GLCanvas = new OpenGL.GlControl();
+            this.panelShaders = new System.Windows.Forms.Panel();
+            this.panelProperties = new System.Windows.Forms.Panel();
+            this.spcMain = new System.Windows.Forms.SplitContainer();
+            this.spcRender = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.spcMain)).BeginInit();
+            this.spcMain.Panel2.SuspendLayout();
+            this.spcMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.spcRender)).BeginInit();
+            this.spcRender.Panel1.SuspendLayout();
+            this.spcRender.SuspendLayout();
             this.SuspendLayout();
             // 
             // GLCanvas
@@ -41,7 +51,7 @@
             this.GLCanvas.Location = new System.Drawing.Point(0, 0);
             this.GLCanvas.MultisampleBits = ((uint)(0u));
             this.GLCanvas.Name = "GLCanvas";
-            this.GLCanvas.Size = new System.Drawing.Size(1264, 681);
+            this.GLCanvas.Size = new System.Drawing.Size(639, 552);
             this.GLCanvas.StencilBits = ((uint)(0u));
             this.GLCanvas.TabIndex = 0;
             this.GLCanvas.ContextCreated += new System.EventHandler<OpenGL.GlControlEventArgs>(this.GLCanvas_ContextCreated);
@@ -49,16 +59,67 @@
             this.GLCanvas.Render += new System.EventHandler<OpenGL.GlControlEventArgs>(this.GLCanvas_Render);
             this.GLCanvas.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GLCanvas_KeyDown);
             // 
+            // panelShaders
+            // 
+            this.panelShaders.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelShaders.Location = new System.Drawing.Point(0, 0);
+            this.panelShaders.Name = "panelShaders";
+            this.panelShaders.Size = new System.Drawing.Size(150, 681);
+            this.panelShaders.TabIndex = 1;
+            // 
+            // panelProperties
+            // 
+            this.panelProperties.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelProperties.Location = new System.Drawing.Point(1114, 0);
+            this.panelProperties.Name = "panelProperties";
+            this.panelProperties.Size = new System.Drawing.Size(150, 681);
+            this.panelProperties.TabIndex = 2;
+            // 
+            // spcMain
+            // 
+            this.spcMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.spcMain.Location = new System.Drawing.Point(150, 0);
+            this.spcMain.Name = "spcMain";
+            // 
+            // spcMain.Panel2
+            // 
+            this.spcMain.Panel2.Controls.Add(this.spcRender);
+            this.spcMain.Size = new System.Drawing.Size(964, 681);
+            this.spcMain.SplitterDistance = 321;
+            this.spcMain.TabIndex = 3;
+            // 
+            // spcRender
+            // 
+            this.spcRender.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.spcRender.Location = new System.Drawing.Point(0, 0);
+            this.spcRender.Name = "spcRender";
+            this.spcRender.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // spcRender.Panel1
+            // 
+            this.spcRender.Panel1.Controls.Add(this.GLCanvas);
+            this.spcRender.Size = new System.Drawing.Size(639, 681);
+            this.spcRender.SplitterDistance = 552;
+            this.spcRender.TabIndex = 0;
+            // 
             // Editor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
             this.ClientSize = new System.Drawing.Size(1264, 681);
-            this.Controls.Add(this.GLCanvas);
+            this.Controls.Add(this.spcMain);
+            this.Controls.Add(this.panelProperties);
+            this.Controls.Add(this.panelShaders);
             this.ForeColor = System.Drawing.Color.White;
             this.Name = "Editor";
             this.Text = "ShaderStudio - Editor";
+            this.spcMain.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.spcMain)).EndInit();
+            this.spcMain.ResumeLayout(false);
+            this.spcRender.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.spcRender)).EndInit();
+            this.spcRender.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -66,6 +127,10 @@
         #endregion
 
         private OpenGL.GlControl GLCanvas;
+        private System.Windows.Forms.Panel panelShaders;
+        private System.Windows.Forms.Panel panelProperties;
+        private System.Windows.Forms.SplitContainer spcMain;
+        private System.Windows.Forms.SplitContainer spcRender;
     }
 }
 
